ca1584b2a1edfef71419df84a4ab15c07fd4e419
Juanse Hurtado <juanse@Juanses-MacBook-Pro.local>
2022-03-21 14:11:35 -0500

    Backend functional

--- api/src/routes/pokemon.js
+++ api/src/routes/pokemon.js
@@ -5,6 +5,7 @@ const { Type } = require('../db')
 const router = Router();
 
 router.get('/', async function (req, res, next) {
+
     // Get pokemon from PokeAPI
     let response = await axios.get("http://pokeapi.co/api/v2/pokemon")
 
@@ -16,20 +17,18 @@ router.get('/', async function (req, res, next) {
         include: Type
     })
 
-    // 
+    // Save all data needed into an array of objects
     Promise.all([pokemonApi, pokemonDb])
         .then(async response => {
             const [pokemonApi, pokemonDb] = response
 
             allPokemonApi = []
-            setLimit = 5
+            setLimit = 12
 
             for (let i = 0; i < setLimit; i++) {
                 const urlRequest = await axios.get(pokemonApi[i].url)
 
-                console.log(urlRequest.data.types)
                 types = urlRequest.data.types.map(poke => {return poke.type.name})
-                console.log(types)
                 
                 
                 var obj = {
@@ -53,22 +52,80 @@ router.get('/', async function (req, res, next) {
         })
 })
 
-// router.get('/', async function (req, res, next) {
-//     return await Pokemon.findAll(
-//         {include: Type}
-//     )
-//         .then(poke => {
-//             res.send(poke)
-//         }).catch(error => {
-//             next(error)
-//         })
+router.get('/name/:name', async (req, res, next) => {
+    const name = req.params.name
 
-//     // const allPokemon = await downloadFromAPI()
-//     // res.status(200).send(allPokemon)
-// })
+    let foundInDb = await Pokemon.findAll({
+        include: Type,
+        where: {
+            name: name
+        }
+    })
 
+    try {
+        let apiResponse = await axios.get("http://pokeapi.co/api/v2/pokemon/" + name)
+        types = apiResponse.data.types.map(poke => {return poke.type.name})
+    
+            var obj = {
+                name: name,
+                hp: apiResponse.data.stats[0].base_stat,
+                attack: apiResponse.data.stats[1].base_stat,
+                defense: apiResponse.data.stats[2].base_stat,
+                speed: apiResponse.data.stats[5].base_stat,
+                weight: apiResponse.data.weight,
+                height: apiResponse.data.height,
+                types: types,
+                img: apiResponse.data.sprites.front_default
+            }
+    } catch(error) {
+        
+    }
 
+    
 
+    let foundInApi = [obj]
+
+    let allFound = [...foundInDb, ...foundInApi]
+
+    let filteredAllFound = allFound.filter(item => item)
+
+    if (allFound.length < 1) return res.send('No pokemon found')
+
+    res.send(filteredAllFound)
+})
+
+router.get('/:id', async (req, res, next) => {
+    try {
+        const id = req.params.id
+        let foundPokemon
+    
+        if(typeof id === 'string' && id.length > 8) {
+            foundPokemon = await Pokemon.findByPk(id)
+            res.send(foundPokemon)
+        } else {
+
+            let response = await axios.get('http://pokeapi.co/api/v2/pokemon/' + id)
+
+            var obj = {
+                name: response.data.name,
+                HP: response.data.stats[0].base_stat,
+                attack: response.data.stats[1].base_stat,
+                defense: response.data.stats[2].base_stat,
+                speed: response.data.stats[5].base_stat,
+                weight: response.data.weight,
+                height: response.data.height,
+                img: response.data.sprites.front_default
+            }
+
+            foundPokemon = obj
+            res.send(foundPokemon)
+        }
+        
+    } catch(error) {
+        next(error)
+    }
+
+})
 
 router.post('/', async function (req, res, next) {
     try {
